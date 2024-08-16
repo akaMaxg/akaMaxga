f6e416e85561bf8c1a1aab9c81da57a423d03aa6
akaMaxg <m.guclu@live.se>
2024-08-16 21:12:27 +0200

    Ny

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hej finis, kolla vad jag kan <3 "/>
   </div>
 </template>
 
